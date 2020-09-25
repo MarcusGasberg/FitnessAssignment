64b2a351f6870186fb4ee8cc1ef0a1030e7ea993
Mathias <mathhan01@gmail.com>
2020-09-25 16:01:09 +0200

    merge continued

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-<<<<<<< HEAD
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-=======
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express", layout: "layout.ejs" });
->>>>>>> e0fbd206dfb59e79ea2bac43064f35a2a0332e35
 });
 
 module.exports = router;
